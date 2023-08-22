f57b313179954df24c129aa3ef73961a493e52c4
Georgina <georginatomass@gmail.com>
2023-08-22 09:19:33 +0200

    components Game, landinf, settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,59 +1,7 @@
 import "./App.css";
-import { Route, Routes, useLocation } from 'react-router-dom'
 
-function App() {
-  
-  const cards = [
-    { name: "aquaman", img: "aquaman.jpg" },
-    { name: "batman", img: "batman.jpg" },
-    { name: "captain america", img: "captain-america.jpg" },
-    { name: "fantastic four", img: "fantastic-four.jpg" },
-    { name: "flash", img: "flash.jpg" },
-    { name: "green arrow", img: "green-arrow.jpg" },
-    { name: "green lantern", img: "green-lantern.jpg" },
-    { name: "ironman", img: "ironman.jpg" },
-    { name: "spiderman", img: "spiderman.jpg" },
-    { name: "superman", img: "superman.jpg" },
-    { name: "the avengers", img: "the-avengers.jpg" },
-    { name: "thor", img: "thor.jpg" },
-    { name: "aquaman", img: "aquaman.jpg" },
-    { name: "batman", img: "batman.jpg" },
-    { name: "captain america", img: "captain-america.jpg" },
-    { name: "fantastic four", img: "fantastic-four.jpg" },
-    { name: "flash", img: "flash.jpg" },
-    { name: "green arrow", img: "green-arrow.jpg" },
-    { name: "green lantern", img: "green-lantern.jpg" },
-    { name: "ironman", img: "ironman.jpg" },
-    { name: "spiderman", img: "spiderman.jpg" },
-    { name: "superman", img: "superman.jpg" },
-    { name: "the avengers", img: "the-avengers.jpg" },
-    { name: "thor", img: "thor.jpg" },
-  ];
-
-
+export const App = () => {
   return (
-    <>
-    
-      <div>
-        <h1>Superhero Memory Game</h1>
-      </div>
-      <div id="memory_board">
-        {cards.map((card, i) => (
-        <div className="card">{card.name}</div>
-        ))}
-           
-      </div>
-      <div id="score">
-        <h2>Score </h2>
-        <p>
-          Pairs Clicked: <span id="pairs_clicked">0</span>
-        </p>
-        <p>
-          Pairs Guessed: <span id="pairs_guessed">0</span>
-        </p>
-      </div>
-    </>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React from "react";
 
 export const Game = () => {
+  const cards = [
+    { name: "aquaman", img: "aquaman.jpg" },
+    { name: "batman", img: "batman.jpg" },
+    { name: "captain america", img: "captain-america.jpg" },
+    { name: "fantastic four", img: "fantastic-four.jpg" },
+    { name: "flash", img: "flash.jpg" },
+    { name: "green arrow", img: "green-arrow.jpg" },
+    { name: "green lantern", img: "green-lantern.jpg" },
+    { name: "ironman", img: "ironman.jpg" },
+    { name: "spiderman", img: "spiderman.jpg" },
+    { name: "superman", img: "superman.jpg" },
+    { name: "the avengers", img: "the-avengers.jpg" },
+    { name: "thor", img: "thor.jpg" },
+    { name: "aquaman", img: "aquaman.jpg" },
+    { name: "batman", img: "batman.jpg" },
+    { name: "captain america", img: "captain-america.jpg" },
+    { name: "fantastic four", img: "fantastic-four.jpg" },
+    { name: "flash", img: "flash.jpg" },
+    { name: "green arrow", img: "green-arrow.jpg" },
+    { name: "green lantern", img: "green-lantern.jpg" },
+    { name: "ironman", img: "ironman.jpg" },
+    { name: "spiderman", img: "spiderman.jpg" },
+    { name: "superman", img: "superman.jpg" },
+    { name: "the avengers", img: "the-avengers.jpg" },
+    { name: "thor", img: "thor.jpg" },
+  ];
   return (
-    <div>Game</div>
-  )
-}
+    <>
+      <div>
+        <h1>Superhero Memory Game</h1>
+      </div>
+      <div id="memory_board">
+        {cards.map((card, i) => (
+          <div className="card">{card.name}</div>
+        ))}
+      </div>
+      <div id="score">
+        <h2>Score </h2>
+        <p>
+          Pairs Clicked: <span id="pairs_clicked">0</span>
+        </p>
+        <p>
+          Pairs Guessed: <span id="pairs_guessed">0</span>
+        </p>
+      </div>
+    </>
+  );
+};
