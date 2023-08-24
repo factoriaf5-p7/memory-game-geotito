9e039c0efc86120def223e010b4f3555546029a3
Georgina <georginatomass@gmail.com>
2023-08-24 12:52:14 +0200

    #3 primera pagina de settings

--- client/src/components/Settings.tsx
+++ client/src/components/Settings.tsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import {useState} from 'react'
 
 export const Settings = () => {
+
+  const [difficulty, setDifficulty] = useState("medium")
+  const [theme, setTheme] = useState("heroes")
+
+  const onOptionChange = e => {
+    setDifficulty(e.target.value)
+  }
+
   return (
-    <div>Settings</div>
+    <>
+      <div>
+        <h1>Superhero Memory Game</h1>
+      </div>
+      <div  id="memory_board">
+        <h1>Settings</h1>
+        <div>Themes ---
+        <input 
+          type="radio" 
+          name="theme" 
+          value="heroes" 
+          id="heroes" 
+          checked={theme === "heroes"}
+          />
+        <label htmlFor="heroes">Heroes</label>
+
+        <input 
+          type="radio" 
+          name="theme" 
+          value="abc" 
+          id="abc" 
+          checked={theme === "abc"}
+          />
+        <label htmlFor="abc">ABC</label>
+        </div>
+        <div>Difficulty ---
+        <input 
+          type="radio" 
+          name="difficulty" 
+          value="easy" 
+          id="easy" 
+          checked={difficulty === "easy"}
+          onChange={onOptionChange}
+          />
+        <label htmlFor="easy">Easy</label>
+        <input 
+          type="radio" 
+          name="difficulty" 
+          value="medium" 
+          id="medium" 
+          checked={difficulty === "medium"}
+          onChange={onOptionChange}
+          />
+        <label htmlFor="medium">Medium</label>
+        <input 
+            type="radio" 
+            name="difficulty" 
+            value="hard" 
+            id="hard"
+            checked={difficulty === "hard"} 
+            onChange={onOptionChange}/>
+        <label htmlFor="hard">Hard</label>
+        </div>
+      </div>
+
+    </>
   )
 }
